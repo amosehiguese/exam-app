ff2ab6e96d691a90a1cdec9206a2694898fa1a05
unknown <amosehiguese@gmail.com>
2023-01-14 09:30:41 +0100

    Implement test_create_user TestCase on User model

--- users/tests.py
+++ users/tests.py
@@ -3,3 +3,20 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+class UserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        now = timezone.now()
+        user = User.objects.create_user(
+            email='testuser@email.com',
+            password='testing123',
+
+        )
+        self.assertEqual(user.email, 'testuser@email.com')
+        self.assertTrue(user.check_password('testing123'))
+        self.assertFalse(user.check_password('wrongpassword'))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_verified)
+        self.assertTrue(user.date_joined)
