dce31628bcf9eff224f190d77686656c54a53932
unknown <amosehiguese@gmail.com>
2023-01-17 20:27:55 +0100

    Add SignUpForm view for users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import User
+from .forms import UserRegistrationForm
 
-# Create your views here.
+class SignUpForm(View):
+    form_class = UserRegistrationForm
+    initial = {'key':'value'}
+    template_name = 'users/sign_up.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
