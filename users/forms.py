15a27f27af04efda5e13486279153afdf9eb5635
unknown <amosehiguese@gmail.com>
2023-01-19 12:58:17 +0100

    Add css styles

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserRegistrationForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput, help_text='Enter at least 8 character', label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput, help_text=None, label='Re-enter Password')
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2')
+
+    def clean_email(self):
+        pass
