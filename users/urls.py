1c231ca8027e0f0b192b35e6e7014944cc13ff5d
unknown <amosehiguese@gmail.com>
2023-01-19 13:13:56 +0100

    Add password-reset url for PasswordResetView

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('sign-up/', views.SignUpForm.as_view(), name='sign-up'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # password reset urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
 ]
