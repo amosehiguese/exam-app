c8f9359b7e9e4d44086f6092ae3913fd3941803b
unknown <amosehiguese@gmail.com>
2023-01-20 11:33:07 +0100

    Add password-reset-confirm url in users urls.py for PasswordResetConfirmView

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # password reset urls
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
+    
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
 
