08384dd448860ec35389a2c9ccbd9ec848f1fe3d
unknown <amosehiguese@gmail.com>
2023-01-22 19:34:29 +0100

    Add PhoneNumberVerificationForm to users forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import PhoneNumber
 
 from django.utils.translation import gettext_lazy as _
 
@@ -20,3 +21,6 @@ class UserRegistrationForm(UserCreationForm):
         except User.DoesNotExist:
             return email
         raise forms.ValidationError(_('A user with that email already exists.'))
+
+class PhoneNumberVerificationForm(forms.Form):
+    phone_number = forms.CharField(max_length=20)
