8b30d260ccb4efaebe72c099c050590113960ff1
unknown <amosehiguese@gmail.com>
2023-01-23 18:39:04 +0100

    Add questions manytomany field to Exam model

--- exam_app/models.py
+++ exam_app/models.py
@@ -6,4 +6,4 @@ class Exam(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    questions = models.ManyToManyField('Question', related_name='exams')
