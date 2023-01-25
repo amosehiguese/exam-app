8fe2743e230895e63359031c5c51bb158d221d70
unknown <amosehiguese@gmail.com>
2023-01-25 15:17:24 +0100

    Add str method to Choice Model

--- exam_app/models.py
+++ exam_app/models.py
@@ -42,3 +42,6 @@ class Choice(models.Model):
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.text
