b36b9818df9fc0dd610088dfbd494f49d0e6a1bc
unknown <amosehiguese@gmail.com>
2023-01-28 07:50:50 +0100

    Add media settings to settings.py

--- exam/settings.py
+++ exam/settings.py
@@ -155,3 +155,8 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
